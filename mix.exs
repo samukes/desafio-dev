f05ec3c22daa3777a92e3e45d8b5db3bd9bdaf95
samukes <darkdarumia@gmail.com>
2022-06-01 19:58:17 -0300

    Feat add exconveralls

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Desafiodev.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6"}
+      {:credo, "~> 1.6"},
+      {:excoveralls, "~> 0.14.5"}
     ]
   end
 
