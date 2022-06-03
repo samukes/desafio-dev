5aaa4c92b48d19b66f37a457100ee8d4cdc87231
samukes <darkdarumia@gmail.com>
2022-06-03 01:08:17 -0300

    Fix report migration

--- priv/repo/migrations/20220602204843_create_table_reports.exs
+++ priv/repo/migrations/20220602204843_create_table_reports.exs
@@ -4,7 +4,7 @@ defmodule Desafiodev.Repo.Migrations.CreateTableReports do
   def change do
     create table(:reports, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :cnab_transacations, :map
+      add :cnab_transactions, :map
 
       timestamps()
     end
