6fef830b1c759e241b8f027bb8b505c5f1859cb8
samukes <darkdarumia@gmail.com>
2022-06-03 01:08:49 -0300

    Fix parse file

--- lib/desafiodev/reports/report.ex
+++ lib/desafiodev/reports/report.ex
@@ -27,34 +27,49 @@ defmodule Desafiodev.Reports.Report do
   defp retrieve_information(%Changeset{changes: %{file_cnab: file_cnab}} = changeset) do
     file_cnab_lines = String.split(file_cnab, "\n")
 
-    cnab_transacations = Enum.map_reduce(file_cnab_lines, [], &map_lines/2)
+    file_cnab_lines =
+      file_cnab_lines
+      |> Enum.map(fn line -> String.trim(line) end)
+      |> Enum.filter(fn line -> String.trim(line) != "" end)
 
-    change(changeset, %{cnab_transacations: cnab_transacations})
+    {_list, cnab_transactions} = Enum.map_reduce(file_cnab_lines, [], &map_lines/2)
+
+    put_change(changeset, :cnab_transactions, cnab_transactions)
   end
 
   defp map_lines(line, acc) do
     {type, operation} = String.slice(line, 0..0) |> match_type()
     {:ok, date} = String.slice(line, 1..8) |> Timex.parse("{YYYY}{0M}{0D}")
-    {value, _string} = String.slice(line, 9..18) |> Float.parse()
-    cpf = String.slice(line, 19..29)
-    card = String.slice(line, 30..41)
+
+    value =
+      String.slice(line, 9..18)
+      |> Decimal.new()
+      |> Decimal.to_float()
+
+    cpf = String.slice(line, 19..29) |> String.trim()
+    card = String.slice(line, 30..41) |> String.trim()
     {:ok, time} = String.slice(line, 42..47) |> Timex.parse("{h24}{0m}{0s}")
-    owner = String.slice(line, 48..61)
-    company = String.slice(line, 62..80)
+    owner = String.slice(line, 48..61) |> String.trim()
+    company = String.slice(line, 62..80) |> String.trim()
 
-    case Enum.find_index(acc, fn item -> item.owner == owner && item.company == company end) do
-      nil ->
-        get_map({type, date, operation, 0, value, cpf, card, time, owner, company})
-        |> struct_acc(nil)
+    acc =
+      case Enum.find_index(acc, fn item -> item.owner == owner && item.company == company end) do
+        nil ->
+          get_map({type, date, operation, 0, value, cpf, card, time, owner, company})
+          |> struct_acc(acc)
 
-      index ->
-        Enum.at(acc, index) |> struct_map(value) |> struct_acc(acc, index)
-    end
+        index ->
+          Enum.at(acc, index)
+          |> struct_map(operation, value)
+          |> struct_acc(acc, index)
+      end
+
+    {line, acc}
   end
 
   defp match_type("1"), do: {"debito", :entrada}
   defp match_type("2"), do: {"boleto", :saida}
-  defp match_type("3"), do: {"financiamento"}
+  defp match_type("3"), do: {"financiamento", :entrada}
   defp match_type("4"), do: {"credito", :entrada}
   defp match_type("5"), do: {"recebimento_emprestimo", :entrada}
   defp match_type("6"), do: {"vendas", :entrada}
@@ -79,11 +94,11 @@ defmodule Desafiodev.Reports.Report do
     }
   end
 
-  defp struct_map(map, value) do
+  defp struct_map(map, operation, value) do
     get_map({
       map.type,
       map.date,
-      map.operation,
+      operation,
       map.balance,
       value,
       map.cpf,
@@ -97,7 +112,7 @@ defmodule Desafiodev.Reports.Report do
   defp struct_acc(map, acc, index \\ nil) do
     case index do
       nil ->
-        [map]
+        acc ++ [map]
 
       index ->
         Enum.map(acc, fn
