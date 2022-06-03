b9b7076a9595a9ef4369a3fc5287692ff36c78e5
samukes <darkdarumia@gmail.com>
2022-06-03 01:09:02 -0300

    Feat add create action schema

--- lib/desafiodev/reports/report_actions/create.ex
+++ lib/desafiodev/reports/report_actions/create.ex
@@ -2,7 +2,9 @@ defmodule Desafiodev.Reports.ReportActions.Create do
   alias Desafiodev.{Repo, Reports.Report}
 
   def call(params) do
-    changeset = Report.changeset(%Report{}, params)
-    Repo.insert(changeset)
+    case File.read(params) do
+      {:ok, content} -> Report.changeset(%Report{}, %{file_cnab: content}) |> Repo.insert()
+      {:error, _reason} -> {:error, "error"}
+    end
   end
 end
