5a464f089ec80174651e69ee97be8a050c262dc7
samukes <darkdarumia@gmail.com>
2022-06-02 21:19:19 -0300

    Feat add table reports migration

---
+++ priv/repo/migrations/20220602204843_create_table_reports.exs
@@ -0,0 +1,12 @@
+defmodule Desafiodev.Repo.Migrations.CreateTableReports do
+  use Ecto.Migration
+
+  def change do
+    create table(:reports, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :cnab_transacations, :array
+
+      timestamps()
+    end
+  end
+end
